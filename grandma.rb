998eac24d19cef30a6dbc3ea85e5fdc69dcd4f02
Francisco barrios <frankybarrios1@icloud.com>
2020-08-25 02:58:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,11 @@
 # speak_to_grandma
+
    def speak_to_grandma 
  speak_to_grandma == "Hi nana how are you"
  end 
  
- if speak_to_grandma
+ if speak_to_grandma 
    puts "HUH?! SPEAK UP, SONNY!"
+ elsif speak_to_grandma == speak_to_grandma.upcase
+ puts "I love you too pumpkin!"
  end
